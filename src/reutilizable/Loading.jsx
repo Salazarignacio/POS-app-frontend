965e31bf5dfd5aca59525f3ce6089ec8fc8ad6fc
Ignacio <ignaciosalazar986@gmail.com>
2026-05-28 16:13:17 -0300

    Update Loading component and styles for improved spinner color and dark mode support

--- src/reutilizable/Loading.jsx
+++ src/reutilizable/Loading.jsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="loading">Cargando Productos
       <div className="d-flex justify-content-center p-4">
-        <Spinner animation="border" variant="warning" style={{ width: "4rem", height: "4rem" }}/>
+        <Spinner animation="border" className="custom-spinner" style={{ width: "4rem", height: "4rem" }}/>
       </div>
     </div>
   );
